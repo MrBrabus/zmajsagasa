d6dd47eefbfeb7deee5ef4993958e4724ef891f1
MrBrabus <brabus.ivan@gmail.com>
2022-02-16 19:30:46 +0100

    test

--- head.php
+++ head.php
@@ -5,7 +5,6 @@
     <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="0; url=https://repo.github.io/folder/index.php">
     <title><?php echo $currentPage; ?></title>
     <link rel="stylesheet" href="https://use.typekit.net/thf2ewz.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
