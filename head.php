dd5392dbdd52bfad911897d77dc768c0aa68f117
MrBrabus <brabus.ivan@gmail.com>
2022-02-28 02:07:45 +0100

    27.02.2022

--- head.php
+++ head.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/main.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
 </head>
 <body>
     
--- js/main.js
+++ js/main.js
@@ -30,4 +30,3 @@ function parallax(e){
         
     });
 }
-
